0e9ad28a8a2ae124ed6aecdec1f07b2b823ff180
kherma <kamil.herma01@gmail.com>
2021-03-13 13:15:02 +0100

    Add basic server

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
+const express = require("express");
+const path = require("path");
 
+const app = express();
+
+app.use((req, res, next) => {
+  res.show = (fileName) => {
+    res.sendFile(path.join(__dirname, `/views/${fileName}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use("/user/", (req, res, next) => {
+  res.show("login.html");
+});
+
+app.get(["/", "/home"], (req, res) => {
+  res.show("index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.show("about.html");
+});
+
+app.use((req, res) => {
+  res.status(404).show("404.html");
+});
+
+app.listen(8000, () => {
+  console.log("Server on port 8000");
+});
